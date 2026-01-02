4e171eb1549c913a34df7882d22ddc91f6b2b976
Elena Poniavina <elnpn.work@gmail.com>
2026-01-02 22:11:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import os
 import asyncio
-import time
 from telegram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from datetime import timezone, timedelta
+from datetime import timezone, timedelta, datetime
 
 # ========================
 # НАСТРОЙКИ
@@ -14,9 +13,9 @@ TOKEN = os.getenv("TOKEN") # токен бота
 CHAT_ID = int(os.getenv("CHAT_ID")) # id чата
 
 # ВРЕМЯ ОТПРАВКИ (по Москве)
-TIMEZONE = timezone(timedelta(hours=5)) # UTC+3
-SEND_HOUR = 23
-SEND_MINUTE = 45
+TIMEZONE = timezone(timedelta(hours=3)) # UTC+3
+SEND_HOUR = 22
+SEND_MINUTE = 15
 
 # ========================
 # БОТ
@@ -24,12 +23,8 @@ SEND_MINUTE = 45
 
 bot = Bot(token=TOKEN)
 
-def main():
-    print("Бот запущен и ждёт время опроса...")
-    print(TOKEN)
-    print(CHAT_ID)
-
-    bot.send_poll(
+async def send_daily_poll():
+    await bot.send_poll(
         chat_id=CHAT_ID,
         question="Во сколько сегодня идем какать?",
         options=[
@@ -48,15 +43,29 @@ def main():
         is_anonymous=False
     )
 
-    time.sleep(300)
+async def main():
+    scheduler = AsyncIOScheduler(timezone=TIMEZONE)
+
+    scheduler.add_job(
+        send_daily_poll,
+        CronTrigger(hour=SEND_HOUR, minute=SEND_MINUTE)
+    )
+
+    scheduler.start()
+    print("Бот запущен и ждёт время опроса...")
+    print(TOKEN)
+    print(CHAT_ID)
     
+    # чтобы бот не завершался
+    while True:
+        await asyncio.sleep(60)
+        now = datetime.now()
+        print(now)
+        print("Ждем...")
+
 if __name__ == "__main__":
 
-    main()
-
-
-
-
+    asyncio.run(main())
 
 
 
